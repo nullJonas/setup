774c3bb12e6b9a23ccdc1221d7b858b61fb85db4
João Víctor Oliveira Paliares <83377242+nullJonas@users.noreply.github.com>
2023-07-17 13:43:09 -0300

    termina script de instalação

--- config/installArch.sh
+++ config/installArch.sh
@@ -3,6 +3,8 @@
 # Configuração
 DEVICE=/dev/nvme0n1
 PRE=p
+HOSTNAME=lenovinho
+MICROCODE=amd-ucode
 
 # Ativa sincronização do relógio
 timedatectl set-ntp true
@@ -45,4 +47,29 @@ arch-chroot /mnt
 # Configura o idioma e o fuso horário
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
+# Configura a rede
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1    localhost    $HOSTNAME" >> /etc/hosts
+echo "::1          localhost    $HOSTNAME" >> /etc/hosts
+echo "127.0.1.1    $HOSTNAME" >> /etc/hosts
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+# Instala o bootloader e microcode
+pacman -S --noconfirm grub-efi-x86_64 efibootmgr $MICROCODE
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Define a senha do root
+echo "Escolha uma senha para o usuário root"
+passwd
+
+# Termina a instalação e desliga o computador
+exit
+umount -R /mnt
+shutdown now
