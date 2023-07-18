87ad63b0a4a0dcc6d5ae9c7708d32b0f7359b386
João Víctor Oliveira Paliares <83377242+nullJonas@users.noreply.github.com>
2023-07-18 02:04:17 -0300

    adiciona echos

--- config/installArch.sh
+++ config/installArch.sh
@@ -7,9 +7,11 @@ HOSTNAME=lenovinho
 MICROCODE=amd-ucode
 
 # Ativa sincronização do relógio
+echo "\e[1;36m Ativa sincronização do relógio \e[m"
 timedatectl set-ntp true
 
 # Particiona o disco
+echo "\e[1;36m Particiona o disco \e[m"
 parted $DEVICE mklabel gpt
 parted $DEVICE mkpart boot fat32 1MiB 512MiB
 parted $DEVICE mkpart swap linux-swap 512MiB 4608MiB
@@ -17,34 +19,42 @@ parted $DEVICE mkpart root ext4 4608MiB 100%
 parted $DEVICE set 1 esp on
 
 # Formata as partições
+echo "\e[1;36m Formata as partições \e[m"
 mkfs.fat -F 32 "$DEVICE""$PRE"1
 mkswap "$DEVICE""$PRE"2
 mkfs.ext4 "$DEVICE""$PRE"3
 
 # Monta as partições
+echo "\e[1;36m Monta as partições \e[m"
 mount "$DEVICE""$PRE"3 /mnt
 mkdir /mnt/boot
 mount "$DEVICE""$PRE"3 /mnt/boot
 
 # Liga o swap
+echo "\e[1;36m Liga o swap \e[m"
 swapon "$DEVICE""$PRE"2
 
 # Seleciona os mirrors mais rápidos para download
+echo "\e[1;36m Seleciona os mirrors mais rápidos para download \e[m"
 curl -o mirrorlist "https://archlinux.org/mirrorlist/?country=BR&use_mirror_status=on"
 sed -e 's/^#Server/Server/' -e '/^#/d' mirrorlist > /etc/pacman.d/mirrorlist.backup
 pacman -S --noconfirm pacman-contrib
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 # Instala o kernel e pacotes básicos
+echo "\e[1;36m Instala o kernel e pacotes básicos \e[m"
 pacstrap /mnt base base-devel linux linux-firmware
 
 # Gera o arquivo fstab
+echo "\e[1;36m Gera o arquivo fstab \e[m"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Faz o chroot
+echo "\e[1;36m Faz o chroot \e[m"
 arch-chroot /mnt
 
 # Configura o idioma e o fuso horário
+echo "\e[1;36m Configura o idioma e o fuso horário \e[m"
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
@@ -53,6 +63,7 @@ echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
 # Configura a rede
+echo "\e[1;36m Configura a rede \e[m"
 echo $HOSTNAME > /etc/hostname
 echo "127.0.0.1    localhost    $HOSTNAME" >> /etc/hosts
 echo "::1          localhost    $HOSTNAME" >> /etc/hosts
@@ -61,15 +72,18 @@ pacman -S --noconfirm networkmanager
 systemctl enable NetworkManager
 
 # Instala o bootloader e microcode
+echo "\e[1;36m Instala o bootloader e microcode \e[m"
 pacman -S --noconfirm grub-efi-x86_64 efibootmgr $MICROCODE
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Define a senha do root
+echo "\e[1;36m Define a senha do root \e[m"
 echo "Escolha uma senha para o usuário root"
 passwd
 
 # Termina a instalação e desliga o computador
+echo "\e[1;36m Termina a instalação e desliga o computador \e[m"
 exit
 umount -R /mnt
 shutdown now
